ba5cf173ff754363cc9f51396298bedc90cb2701
shinyousong <66022429+shinyousong@users.noreply.github.com>
2020-09-15 18:52:00 +0900

    3-1주차 창연공 과제

---
+++ sketch_sep14a2/sketch_sep14a2.ino
@@ -0,0 +1,28 @@
+#define PIN_LED 13
+unsigned int count, toggle;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200);
+  while (!Serial) {
+    ;
+  }
+  Serial.println("Hello world!");
+  count = toggle = 0;
+  digitalWrite(PIN_LED, toggle); //LED OFF.
+}
+
+void loop() {
+  Serial.println(++count);
+  toggle = toggle_state(toggle); //toogle LED value.
+  
+  digitalWrite(PIN_LED, toggle); //update LED.
+  delay(1000);
+  toggle += 1; //update LED value=1.
+  digitalWrite(PIN_LED, toggle); //update LED.
+  delay(1000);
+  toggle -= 1; //update LED value=0.
+}
+int toggle_state(int toggle) {
+  return toggle;
+}
